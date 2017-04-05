926e3d07ca7225e10dec692752624db5abbf09b3
joegajeckyj <joe.gajeckyj@centurylink.com>
2017-04-05 11:57:42 +0100

    Update winrm_mem.py

--- winrm_mem.py
+++ winrm_mem.py
@@ -19,7 +19,7 @@ print args.user
 print args.password
 
 ps_script = open('scripts/mem.ps1','r').read()
-session = winrm.Session(''args.server'', auth=(''args.user'', ''args.password''))
+session = winrm.Session('GB3GOCHJRGTST02', auth=('administrator', 'dbghYp9e3bR'))
 run = session.run_ps(ps_script)
 print run.status_code
 print run.std_out
