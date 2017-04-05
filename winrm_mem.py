2709bbc22adfd5ddfc273f195bd76a2f3cd0e27d
joegajeckyj <joe.gajeckyj@centurylink.com>
2017-04-05 09:53:59 +0100

    Update winrm_mem.py

--- winrm_mem.py
+++ winrm_mem.py
@@ -3,7 +3,7 @@
 import winrm
 
 ps_script = open('scripts/mem.ps1','r').read()
-s = winrm.Session('10.152.12.28', auth=("{{user}}", "{{password}}"))
+s = winrm.Session("{{server_name}}", auth=("{{user}}", "{{password}}"))
 r = s.run_ps(ps_script)
 print r.status_code
 print r.std_out
