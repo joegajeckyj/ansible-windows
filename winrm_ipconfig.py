7abb027f7ef941b6dfb77830b71f8ecc26975bea
joegajeckyj <joe.gajeckyj@centurylink.com>
2017-04-19 11:03:28 +0100

    Update winrm_ipconfig.py

--- winrm_ipconfig.py
+++ winrm_ipconfig.py
@@ -2,7 +2,7 @@
 
 import winrm
 
-s = winrm.Session('10.105.13.25', auth=('administrator@jrg.lab', 'dbghYp9e3bR'))
+s = winrm.Session('GB3GOCHJRGTST01', auth=('administrator@jrg.lab', 'dbghYp9e3bR'))
 r = s.run_cmd('ipconfig', ['/all'])
 print r.status_code
 print r.std_out
