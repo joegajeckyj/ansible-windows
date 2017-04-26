55ba2bd23b30303548f4968f0288fa27bb423e41
joegajeckyj <joe.gajeckyj@centurylink.com>
2017-04-26 10:39:51 +0100

    Create InstallVMTools.ps1

---
+++ scripts/InstallVMTools.ps1
@@ -0,0 +1,5 @@
+If (!(Test-Path "C:\Program Files\VMware")){
+  MD -force "C:\Program Files\VMware"
+}
+
+Start-Process -NoNewWindow -Wait -FilePath ".\setup64.exe" -ArgumentList "/S /v ""/qn /l*v ""C:\Program Files\VMware\vmmsi.log"" REBOOT=R"""
