abd85d4f3f6065f5e0d0200ef4080645fec10dfc
joegajeckyj <joe.gajeckyj@centurylink.com>
2017-06-09 15:43:50 +0100

    Create process.ps1

--- scripts/process.ps1
+++ scripts/process.ps1
@@ -1,2 +1,2 @@
-Copy-Item -Path \\GB3GOCHJRGTST03\c$\sysadmin\Runner -Destination C:\sysadmin\Runner -Force -Recurse
+Copy-Item -Path \\GB3GOCHJRGTST03\c$\sysadmin\Runner -Destination C:\sysadmin\Runner -Force -Recurse -PassThru
 Get-Process | sort WS -Descending | select -first 10
