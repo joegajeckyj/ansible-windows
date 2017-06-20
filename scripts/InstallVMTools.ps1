aeb0a6046d9186e4aff8a17d638bc9957d1db09b
joegajeckyj <joe.gajeckyj@centurylink.com>
2017-06-20 13:32:03 +0100

    Update InstallVMTools.ps1

--- scripts/InstallVMTools.ps1
+++ scripts/InstallVMTools.ps1
@@ -2,4 +2,4 @@ If (!(Test-Path "C:\Program Files\VMware")){
   MD -force "C:\Program Files\VMware"
 }
 Copy-Item -Path i:\ -Destination c:\sysadmin\Runner -Force -recurse
-Start-Process -NoNewWindow -Wait -FilePath ".\setup64.exe" -ArgumentList "/S /v ""/qn /l*v ""C:\Program Files\VMware\vmmsi.log"" REBOOT=R"""
+Start-Process -NoNewWindow -Wait -FilePath "c:\sysadmin\Runner\setup64.exe" -ArgumentList "/S /v ""/qn /l*v ""C:\Program Files\VMware\vmmsi.log"" REBOOT=R"""
